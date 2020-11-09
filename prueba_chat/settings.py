c71a426b03e3dc9a4b449d723166cf23aa80839c
jpvivas <jpvivas@opalo.com.co>
2020-11-08 22:24:04 -0500

    ajuste

--- prueba_chat/settings.py
+++ prueba_chat/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE_CLASSES = (
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     'whitenoise.middleware.WhiteNoiseMiddleware',
-}
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'prueba_chat.urls'
 
