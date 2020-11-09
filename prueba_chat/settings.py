70435236e550b60081c23bb742eba560f87b12bc
jpvivas <jpvivas@opalo.com.co>
2020-11-08 22:23:26 -0500

    ajuste

--- prueba_chat/settings.py
+++ prueba_chat/settings.py
@@ -71,7 +71,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'prueba_chat.urls'
 
 TEMPLATES = [
